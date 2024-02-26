0b6b5e60f317f33e3810579860251acebe8a3920
RoKerjea <kerjeanr@gmail.com>
2024-02-26 13:39:09 +0100

    chapter 3 image work + changed videos example

--- video-streaming/src/index.js
+++ video-streaming/src/index.js
@@ -3,13 +3,19 @@ const fs = require('fs');
 
 const app = express();
 
-if (!process.env.PORT) {
-	throw new Error("Please specify the port number for the HTTP server with the environment variable PORT.");
-}
-const port = process.env.PORT;
+// if (!process.env.PORT) {
+// 	throw new Error("Please specify the port number for the HTTP server with the environment variable PORT.");
+// }
+// const port = process.env.PORT;
+
+const port = 3000;
+
+app.get('/', (req, res) => {
+	res.send('Hello World!');
+});
 
 app.get('/video', (req, res) => {
-	const path = "./videos/SampleVideo_1280x720_1mb.mp4";
+	const path = "./videos/video_of_a_creepy_animal (1080p).mp4";
 	fs.stat(path, (err, stats) => {
 		if (err) {
 			console.error('An error occurred: ', err);
@@ -27,5 +33,5 @@ app.get('/video', (req, res) => {
 });
 
 app.listen(port, () => {
-	console.log(`Example app listening at http://localhost:${port}, point your browser at http://localhost:3000/video`);
+	console.log(`Example app listening at http://localhost:${port}, point your browser at http://localhost:${port}/video`);
 });
