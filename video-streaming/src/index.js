dd241afbe6464961ad69465272ee3080d5a57c81
RoKerjea <kerjeanr@gmail.com>
2024-02-26 20:19:15 +0100

    page 104

--- video-streaming/src/index.js
+++ video-streaming/src/index.js
@@ -1,14 +1,36 @@
 const express = require('express');
-const fs = require('fs');
+const http = require("http");
 
 const app = express();
 
-if (!process.env.PORT) {
-	throw new Error("Please specify the port number for the HTTP server with the environment variable PORT.");
-}
-const port = process.env.PORT;
 
-// const port = 3000;
+//
+// Throws an error if the any required environment variables are missing.
+//
+
+if (!process.env.PORT) {
+    throw new Error("Please specify the port number for the HTTP server with the environment variable PORT.");
+}
+
+if (!process.env.VIDEO_STORAGE_HOST) {
+    throw new Error("Please specify the host name for the video storage microservice in variable VIDEO_STORAGE_HOST.");
+}
+
+if (!process.env.VIDEO_STORAGE_PORT) {
+    throw new Error("Please specify the port number for the video storage microservice in variable VIDEO_STORAGE_PORT.");
+}
+
+//
+// Extracts environment variables to globals for convenience.
+//
+const PORT = process.env.PORT;
+const VIDEO_STORAGE_HOST = process.env.VIDEO_STORAGE_HOST;
+const VIDEO_STORAGE_PORT = parseInt(process.env.VIDEO_STORAGE_PORT);
+console.log(`Forwarding video requests to ${VIDEO_STORAGE_HOST}:${VIDEO_STORAGE_PORT}.`);
+
+//
+// Registers a HTTP GET route for video streaming.
+//
 
 app.get('/', (req, res) => {
 	res.send('Hello World!');
@@ -31,7 +53,9 @@ app.get('/video', (req, res) => {
 		//node.js streams are readable, writable, or both
 	});
 });
-
-app.listen(port, () => {
-	console.log(`Example app listening at http://localhost:${port}, point your browser at http://localhost:${port}/video`);
+//
+// Starts the HTTP server.
+//
+app.listen(PORT, () => {
+    console.log(`Microservice online`);
 });
